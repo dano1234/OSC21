2a148fa4a23117f3e479484994d083b848e3b347
dano1234 <dan.osullivan@nyu.edu>
2021-02-01 18:21:27 -0500

    ml5 folder workikng on placement of convene windows

--- Awake/AwakeML5andThree/ml5.js
+++ Awake/AwakeML5andThree/ml5.js
@@ -2,72 +2,45 @@
 let camera3D, scene, renderer, cube;
 let dir = 0.01;
 let myCanvas, myVideo, p5CanvasTexture;
-let connections = [];
-let textures = []
 
-function setup() {
-    myCanvas = createCanvas(512, 512);
-    myCanvas.hide();
-    myVideo = createCapture(VIDEO, videoLoaded);
-    myVideo.hide()
-    myVideo.size(320, 240);
- 
-    init3D();
+function setup(){
+  myCanvas = createCanvas(512,512);
+  myCanvas.hide();
+  myVideo = createCapture(VIDEO);
+  myVideo.size(320,240);
+  myVideo.hide();
+
+  init3D();
 }
 
-function videoLoaded(stream) {;
-    let p5lm = new p5LiveMedia(this, "CAPTURE", stream, "mycrazyroomname")
-    p5lm.on('stream', gotStream);
-}
-
-function gotStream(stream, id) {
-    creatNewVideoObject(stream);
-}
-
-function creatNewVideoObject(canvas) {  //this is for remote and local
-    var videoGeometry = new THREE.PlaneGeometry(512, 512);
-    let p5CanvasTexture = new THREE.Texture(canvas.elt);  //NOTICE THE .elt  this give the element
-    //let videoMaterial = new THREE.MeshBasicMaterial({ map:   p5CanvasTexture});
-    let videoMaterial = new THREE.MeshBasicMaterial({ map: p5CanvasTexture, transparent: true, opacity: 1,  side: THREE.DoubleSide });
-    videoMaterial.map.minFilter = THREE.LinearFilter;  //otherwise lots of errors
-    //  let videoMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    myAvatarObj = new THREE.Mesh(videoGeometry, videoMaterial);
-
-    scene.add(myAvatarObj);
-    connections.push(myAvatarObj);
-    textures.push(p5CanvasTexture);
-    console.log(myAvatarObj.material.map);
-    let horiz_angle = (connections.length +1) * 2*Math.PI / 5;
-    let vert_anagle = 0;
-    let distanceFromCenter = 850;
-    x = distanceFromCenter * Math.sin(horiz_angle); //Math.sin(vert_anagle) * 
-    y = 0; //distanceFromCenter  * Math.cos(vert_anagle);
-    z = distanceFromCenter  * Math.cos(horiz_angle); //Math.sin(vert_anagle) * 
-    myAvatarObj.position.set(x,y,z);
-    myAvatarObj.lookAt(0, 0, 0);
-
-}
-
-function draw() {
-    clear();//for making background transparent
-    image(myVideo, (myCanvas.width - myVideo.width) / 2, (myCanvas.height - myVideo.height) / 2);
+function draw(){
+   clear();
+    image(myVideo,(myCanvas.width-myVideo.width)/2,(myCanvas.height-myVideo.height)/2);
 }
 
 function init3D() {
     scene = new THREE.Scene();
-    camera3D = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera3D = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // const geometry = new THREE.BoxGeometry();
-    //  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    //  cube = new THREE.Mesh(geometry, material);
-    //   scene.add(cube);
+   // const geometry = new THREE.BoxGeometry();
+  //  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  //  cube = new THREE.Mesh(geometry, material);
+ //   scene.add(cube);
+
+    var videoGeometry = new THREE.PlaneGeometry(512, 512);
+    p5CanvasTexture = new THREE.Texture(myCanvas.elt);  //NOTICE THE .elt  this give the element
+ //  let videoMaterial = new THREE.MeshBasicMaterial({ map: videoTexture});
+   let videoMaterial = new THREE.MeshBasicMaterial({ map: p5CanvasTexture, transparent: true, opacity: 1, side: THREE.DoubleSide });
+ //  let videoMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+   myAvatarObj = new THREE.Mesh(videoGeometry, videoMaterial);
+    myAvatarObj.position.set(0,0,-500);
+    scene.add(myAvatarObj);
 
 
-    creatNewVideoObject(myCanvas);
 
     let bgGeometery = new THREE.SphereGeometry(900, 100, 40);
     //let bgGeometery = new THREE.CylinderGeometry(725, 725, 1000, 10, 10, true)
@@ -79,26 +52,23 @@ function init3D() {
 
     let back = new THREE.Mesh(bgGeometery, backMaterial);
     scene.add(back);
-
+    
     moveCameraWithMouse();
 
-    camera3D.position.z = 0;
+    camera3D.position.z = 5;
     animate();
 }
 
 function animate() {
-
+ 
     requestAnimationFrame(animate);
-    for (var i = 0; i < textures.length; i++) {
-        textures[i].needsUpdate = true;
-    }
-    // p5CanvasTexture.needsUpdate = true;
+    p5CanvasTexture.needsUpdate = true;
     //cube.scale.x += dir;
-    // cube.scale.y += dir;
+   // cube.scale.y += dir;
     //cube.scale.z += dir;
-    // if (cube.scale.x > 4 || cube.scale.x < -4) {
+   // if (cube.scale.x > 4 || cube.scale.x < -4) {
     //    dir = -dir;
-    // }
+   // }
     renderer.render(scene, camera3D);
 }
 
