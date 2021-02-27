2399fdd8399646c6b05601635ce9b43ea2f003e7
dano1234 <dan.osullivan@nyu.edu>
2021-02-27 17:00:30 -0500

    handpos select

--- Body/Draw/draw.js
+++ Body/Draw/draw.js
@@ -11,6 +11,75 @@ let video;
 let predictions = [];
 
 
+var line;
+var count = 0;
+var mouse = new THREE.Vector3();
+
+init();
+animate();
+
+
+
+//FROM EXAMPLE https://jsfiddle.net/wilt/a21ey9y6/
+function initLine() {
+
+  // geometry
+  var geometry = new THREE.BufferGeometry();
+  var MAX_POINTS = 500;
+  positions = new Float32Array(MAX_POINTS * 3);
+  geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+  // material
+  var material = new THREE.LineBasicMaterial({
+    color: 0xff0000,
+    linewidth: 2
+  });
+
+  // line
+  line = new THREE.Line(geometry, material);
+  scene.add(line);
+
+}
+
+// update line
+function updateLine() {
+  positions[count * 3 - 3] = mouse.x;
+  positions[count * 3 - 2] = mouse.y;
+  positions[count * 3 - 1] = mouse.z;
+  line.geometry.attributes.position.needsUpdate = true;
+}
+
+// mouse move handler
+function onMouseMove(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  mouse.z = 0;
+  mouse.unproject(camera);
+  if( count !== 0 ){
+  	updateLine();
+  }
+}
+
+// add point
+function addPoint(event){
+  console.log("point nr " + count + ": " + mouse.x + " " + mouse.y + " " + mouse.z);
+  positions[count * 3 + 0] = mouse.x;
+  positions[count * 3 + 1] = mouse.y;
+  positions[count * 3 + 2] = mouse.z;
+  count++;
+  line.geometry.setDrawRange(0, count);
+  updateLine();
+}
+
+// mouse down handler
+function onMouseDown(evt) {
+  // on first click add an extra point
+  if( count === 0 ){
+      addPoint();
+  }
+  addPoint();
+}
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(640,480);
